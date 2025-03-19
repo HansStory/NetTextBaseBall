4882d995edd08a54a2a34a609ca7581709de18f3
Han jeong hyeok <gkswjdgur552@gmail.com>
2025-03-19 12:51:07 +0900

    Initial Commit

---
+++ Source/SampleChat/TestBlueprintFunctionLibrary.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "TestBlueprintFunctionLibrary.generated.h"
+
+
+UCLASS()
+class SAMPLECHAT_API UTestBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
