49fc02467bb895da3bdb458f2d8d05cf1961cfbc
Han jeong hyeok <gkswjdgur552@gmail.com>
2025-03-28 20:59:26 +0900

    Remove Not Method

--- Source/SampleChat/Baseball/BaseballGameState.cpp
+++ Source/SampleChat/Baseball/BaseballGameState.cpp
@@ -1,4 +1,4 @@
-#include "Baseball/BaseballGameState.h"
+﻿#include "Baseball/BaseballGameState.h"
 
 #include "Baseball/BaseballGameMode.h"
 #include "Baseball/BaseballPlayerState.h"
@@ -11,56 +11,13 @@ ABaseballGameState::ABaseballGameState()
 	bReplicates = true;
 }
 
+void ABaseballGameState::BeginPlay()
+{
+
+}
+
 void ABaseballGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
-	DOREPLIFETIME(ABaseballGameState, JoinedPlayerStates);
-	DOREPLIFETIME(ABaseballGameState, JoinedPlayerControllers);
-}
-
-void ABaseballGameState::JoinGame(ABaseballPlayerState* JoinPlayerState, const FString& NewPlayerName)
-{
-	JoinPlayerState->SetPlayerName(NewPlayerName);
-
-	UE_LOG(LogTemp, Warning, TEXT("NewPlayer Name : %s"), *NewPlayerName);
-
-	JoinedPlayerStates.Add(JoinPlayerState);
-	ABaseballController* BaseballPC = JoinPlayerState->GetBaseballController();
-	if (BaseballPC)
-	{
-		JoinedPlayerControllers.Add(BaseballPC);
-		BaseballPC->Client_JoinGame(JoinedPlayerStates.Num() == 1);
-	}
-
-	if (ABaseballGameMode* BaseballGM = Cast<ABaseballGameMode>(GetWorld()->GetAuthGameMode()))
-	{
-		//BaseballGM->SetRequiredReadyCount(JoinedPlayerStates.Num());
-	}
-
-	UpdateOtherPlayer();
-}
-
-void ABaseballGameState::UpdateOtherPlayer()
-{
-	for (const ABaseballController* BaseballPC : JoinedPlayerControllers)
-	{
-		if (BaseballPC)
-		{
-			int32 Index = 1;
-			for (const ABaseballPlayerState* BaseballPS : JoinedPlayerStates)
-			{
-				if (BaseballPS)
-				{
-					int32 TargetIndex = 0;
-					if (BaseballPS != BaseballPC->PlayerState)
-					{
-						TargetIndex = Index++;
-					}
-
-					BaseballPC->Client_UpdateOtherPlayerName(TargetIndex, BaseballPS->GetPlayerName());
-				}
-			}
-		}
-	}
 }
--- Source/SampleChat/Baseball/BaseballGameState.h
+++ Source/SampleChat/Baseball/BaseballGameState.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+癤�// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -14,25 +14,15 @@ UCLASS()
 class SAMPLECHAT_API ABaseballGameState : public AGameStateBase
 {
 	GENERATED_BODY()
-	
-	
+
 public:
 	ABaseballGameState();
 
+protected:
+	virtual void BeginPlay() override;
+	
+public:
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
-	// 게임 참가
-	void JoinGame(ABaseballPlayerState* JoinPlayerState, const FString& NewPlayerName);
-	// 다른 플레이어 참가
-	void UpdateOtherPlayer();
-
-	UPROPERTY(Replicated)
-	TArray<TObjectPtr<ABaseballController>> JoinedPlayerControllers;
-
 private:
-	UPROPERTY(Replicated)
-	TArray<TObjectPtr<ABaseballPlayerState>> JoinedPlayerStates;
-
-	UPROPERTY(Replicated)
-	int32 CurrentTurnPlayerIndex = 0;
 };
